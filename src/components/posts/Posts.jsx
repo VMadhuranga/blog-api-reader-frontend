a39a0cfd0fa62126a736b68386f130790a6343b4
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-18 14:25:13 +0530

    Create initial Posts component

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,15 @@
+export default function Posts() {
+  return (
+    <section>
+      <h2>Posts</h2>
+      <article>
+        <ul>
+          <li>
+            <a href="">Post title</a>
+            <span>Post created date</span>
+          </li>
+        </ul>
+      </article>
+    </section>
+  );
+}
