021fd4ed48bd012c88f9b58feb37652fb642cb7c
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-19 08:02:21 +0530

    Create initial browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Posts from "./components/posts/Posts.jsx";
+import ErrorPage from "./components/error-page/ErrorPage.jsx";
+import Post from "./components/post/Post.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/posts",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Posts />,
+      },
+      {
+        path: ":post_id",
+        element: <Post />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
