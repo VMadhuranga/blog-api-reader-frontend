73a44cc55fe017d2b8f9c90a2d0a9d925d44e528
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-19 11:20:28 +0530

    Use postsLoader data to render posts links

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,13 +1,21 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 export default function Posts() {
+  const posts = useLoaderData();
+
   return (
     <section>
       <h2>Posts</h2>
       <article>
         <ul>
-          <li>
-            <a href="">Post title</a>
-            <span>Post created date</span>
-          </li>
+          {posts.map((post) => {
+            return (
+              <li key={post._id}>
+                <Link to={`/posts/${post._id}`}>{post.title}</Link>
+                <span>{new Date(post.createdDate).toLocaleDateString()}</span>
+              </li>
+            );
+          })}
         </ul>
       </article>
     </section>
