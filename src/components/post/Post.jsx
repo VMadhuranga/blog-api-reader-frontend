af634260401dc27eea5b6c76624a56de068afa92
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-19 14:06:32 +0530

    Add Outlet component and use postLoader data to render post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,14 +1,16 @@
-import Comments from "../comments/Comments";
+import { Outlet, useLoaderData } from "react-router-dom";
 
 export default function Post() {
+  const post = useLoaderData();
+
   return (
     <>
       <section>
-        <h2>Post title</h2>
-        <p>Post created date</p>
-        <article>Post content</article>
+        <h2>{post.title}</h2>
+        <p>{new Date(post.createdDate).toLocaleDateString()}</p>
+        <article>{post.content}</article>
       </section>
-      <Comments />
+      <Outlet />
     </>
   );
 }
