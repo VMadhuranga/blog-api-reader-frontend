8066ea7accd2854142554c7f1154923c6177b19d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-21 13:29:56 +0530

    Add page loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const navigation = useNavigation();
+
   return (
     <>
       <header>
         <h1>Blog API</h1>
       </header>
       <main>
-        <Outlet />
+        {navigation.state === "loading" ? <h3>Loading...</h3> : <Outlet />}
       </main>
       <footer>&copy; 2024 VMadhuranga</footer>
     </>
