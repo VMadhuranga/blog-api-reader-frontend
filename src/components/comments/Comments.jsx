0eb9cf08e7cf3ff5174a69af565b382e5921dc38
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-21 08:29:03 +0530

    Update Comment component to show form fields error messages

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,24 +1,41 @@
-import { useLoaderData } from "react-router-dom";
+import { Form, useActionData, useLoaderData } from "react-router-dom";
 
 export default function Comments() {
   const comments = useLoaderData();
+  const error = useActionData();
 
   return (
     <section>
       <h3>Comments</h3>
-      <form>
+      <Form method="post">
         <div>
-          <label htmlFor="user_name">Name:</label>
-          <input type="text" name="user_name" id="user_name" />
+          <label htmlFor="user_name">Name :</label>
+          <input type="text" name="user_name" id="user_name" required />
+          {error && (
+            <span>
+              {error.data.find((data) => data.path === "user_name").msg}
+            </span>
+          )}
         </div>
         <div>
-          <label htmlFor="comment">Name:</label>
-          <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+          <label htmlFor="comment">Comment :</label>
+          <textarea
+            name="comment"
+            id="comment"
+            cols="30"
+            rows="10"
+            required
+          ></textarea>
+          {error && (
+            <span>
+              {error.data.find((data) => data.path === "comment").msg}
+            </span>
+          )}
         </div>
         <div>
           <button type="submit">Submit</button>
         </div>
-      </form>
+      </Form>
       <article>
         <ul>
           {comments.map((comment) => {
