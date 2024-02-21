5b133a0cce320a241ee1b35411a11d396e1be08b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-21 13:28:18 +0530

    Add Go back to Posts link
    Additional:
    - Change h1 tag to h2

--- src/components/error-page/ErrorPage.jsx
+++ src/components/error-page/ErrorPage.jsx
@@ -1,14 +1,17 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
 
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
+      <h2>Oops!</h2>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
+      </p>
+      <p>
+        Go back to <Link to={"/posts"}>Posts</Link>
       </p>
     </div>
   );
