8cd5f1cb64e7565d0f0f4e17df9c3b290616d98b
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-26 09:37:18 +0530

    Create unescape function

---
+++ src/utils/unescape.js
@@ -0,0 +1,15 @@
+export default function unescape(string) {
+  const characters = [
+    ["&lt;", "<"],
+    ["&gt;", ">"],
+    ["&amp;", "&"],
+    ["&#x27;", "'"],
+    ["&quot;", '"'],
+    ["&#x2F;", "/"],
+  ];
+
+  return characters.reduce(
+    (acc, [key, value]) => acc.replaceAll(key, value),
+    string,
+  );
+}
