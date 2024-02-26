1734b6c27c680baeca048a20dcea7b21b6c01771
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-26 14:28:00 +0530

    Add Posts component styles

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,26 +1,27 @@
 import { Link, useLoaderData } from "react-router-dom";
 import unescape from "../../utils/unescape";
+import styles from "./Posts.module.css";
 
 export default function Posts() {
   const posts = useLoaderData();
 
   return (
-    <section>
+    <section className={styles.postsComponent}>
       <h2>Posts</h2>
       <article>
         {posts.length > 0 ? (
-          <ul>
+          <ul className={styles.postsList}>
             {posts.map((post) => {
               return (
                 <li key={post._id}>
                   <Link to={`${post._id}`}>{unescape(post.title)}</Link>
-                  <span>{new Date(post.createdDate).toLocaleDateString()}</span>
+                  <span>{new Date(post.createdDate).toDateString()}</span>
                 </li>
               );
             })}
           </ul>
         ) : (
-          <p>There are no posts yet</p>
+          <p className={styles.noPostMsg}>There are no posts yet</p>
         )}
       </article>
     </section>
