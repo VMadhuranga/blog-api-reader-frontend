7ec859f32666b261b45102c673aa37049e8847bf
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-27 11:04:18 +0530

    Add ErrorPage component styles

--- src/components/error-page/ErrorPage.jsx
+++ src/components/error-page/ErrorPage.jsx
@@ -1,10 +1,11 @@
 import { Link, useRouteError } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
 
 export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div id="error-page">
+    <div id="error-page" className={styles.errorPage}>
       <h2>Oops!</h2>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
