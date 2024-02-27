4b848e459a265c47be792745a4a6182341c67463
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-02-27 13:58:27 +0530

    Use fetcher to stop causing navigation
    Additional:
    - Add state to form fields to clear values when submitting

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,18 +1,38 @@
-import { Form, useActionData, useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { useActionData, useFetcher, useLoaderData } from "react-router-dom";
 import unescape from "../../utils/unescape";
 import styles from "./Comments.module.css";
 
 export default function Comments() {
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
   const comments = useLoaderData();
   const errors = useActionData();
+  const fetcher = useFetcher();
+
+  function handleFormSubmit() {
+    setName("");
+    setComment("");
+  }
 
   return (
     <section className={styles.commentSection}>
       <h3>Comments</h3>
-      <Form method="post" className={styles.createCommentForm}>
+      <fetcher.Form
+        method="post"
+        className={styles.createCommentForm}
+        onSubmit={handleFormSubmit}
+      >
         <div>
           <label htmlFor="user_name">Name :</label>
-          <input type="text" name="user_name" id="user_name" required />
+          <input
+            type="text"
+            name="user_name"
+            id="user_name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
           {errors &&
             errors.data
               .filter((error) => error.path === "user_name")
@@ -25,6 +45,8 @@ export default function Comments() {
             id="comment"
             cols="30"
             rows="10"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
             required
           ></textarea>
           {errors &&
@@ -35,7 +57,7 @@ export default function Comments() {
         <div>
           <button type="submit">Submit</button>
         </div>
-      </Form>
+      </fetcher.Form>
       <article>
         {comments.length > 0 ? (
           <ul className={styles.commentList}>
