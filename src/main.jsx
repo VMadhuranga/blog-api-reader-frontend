9fc8625dd558599a7b3ad0b5d88eb7a050bf8881
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-03-02 09:35:54 +0530

    Remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import {
   createBrowserRouter,
   redirect,
   RouterProvider,
-  useLocation,
 } from "react-router-dom";
 import App from "./App.jsx";
 import Posts from "./components/posts/Posts.jsx";
